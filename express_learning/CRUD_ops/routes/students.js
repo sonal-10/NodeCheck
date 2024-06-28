8a4017c8f4a2ec4cd0a598b8cd7c30c3cdcc870b
sonal_monis <sonal.monis@7edge.com>
2024-06-28 10:10:39 +0530

    Completed_CRUD

--- express_learning/CRUD_ops/routes/students.js
+++ express_learning/CRUD_ops/routes/students.js
@@ -11,134 +11,159 @@ function studentRouter(client) {
 
     // GET METHOD
     router.get('/', async (req, res) => {
-        if (req.method === "GET") {
-            try {
-                const students = await DB_COLLECTIONS.find().toArray();
-                res.json(students);
-            }
-            catch (erroror) {
-                res.status(500).json({
-                    message: erroror.message
-                });
-            }
-        }
-    });
+        if(req.method === "GET"){
+            try{
+                const students = await DB_COLLECTIONS
+                .find().toArray();
+                // .sort({name:1});
 
-
-    // GET one student
-    router.get('/:id', async (req, res) => {
-        const id = req.params.id;
-        if (req.method === "GET") {
-            try {
-                const student = await DB_COLLECTIONS.findOne({
-                    _id: new ObjectId(id)
-                });
-                if (student === null) {
-                    return res.status(404).json({
-                        message: "Could not Find Student"
-                    });
-                }
-                res.json(student);
+                res.status(200).json(students);
             }
-            catch (erroror) {
-                res.status(500).json({
-                    message: erroror.message
+            catch(error){
+                res.status(500)
+                .json({
+                    message:error.message
                 });
             }
         }
     });
+    
 
-    // POST Request
-    router.post('/', async (req, res) => {
-        if (req.method === "POST") {
-            const student = {
-                name: req.body.name,
-                scores: req.body.scores
-            };
-            console.log(student);
-            try {
-                const result = await DB_COLLECTIONS.insertOne(student);
-                console.log(result);
-                res.status(201).json(
-                    result
-                );
-            } catch (error) {
-                res.status(400).json({
-                    message: error.message
+// GET one student
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    if (req.method === "GET" && ObjectId.isValid(id)) {
+        try {
+            const student = await DB_COLLECTIONS.findOne({
+                _id: new ObjectId(id)
+            });
+            if (student === null) {
+                return res.status(404).json({
+                    message: "Could not Find Student"
                 });
             }
+            res.status(200).json(student);
         }
-    });
-
-    // PUT Request
-    router.put('/:id', async (req, res) => {
-        if (req.method === "PUT") {
-            const id = req.params.id;
-            const updates = {
-                name: req.body.name,
-                scores: req.body.scores
-            };
-            try {
-                const result = await DB_COLLECTIONS.updateOne(
-                    { _id: new ObjectId(id) },
-                    { $set: updates }
-                );
-                if (result.matchedCount === 0) {
-                    return res.status(404).json({
-                        message: 'Cannot find student'
-                    });
-                }
-
-                const student = await DB_COLLECTIONS.findOne({
-                    _id: new ObjectId(id)
-                });
-                if (student === null) {
-                    return res.status(404).json({
-                        message: "Could not Find Student"
-                    });
-                }
-                res.json(student);
-            }
-            catch (error) {
-                res.status(400).json({
-                    message: error.message
-                });
-            }
+        catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
         }
-    });
+    }
+});
 
-    // PATCH Request
-    router.patch('/:id', async(req,res)=>{
-        if(req.method === "PATCH"){
-            
+// POST Request
+router.post('/', async (req, res) => {
+    if (req.method === "POST") {
+        const student = {
+            name: req.body.name,
+            scores: req.body.scores
+        };
+        console.log(student);
+        try {
+            const result = await DB_COLLECTIONS.insertOne(student);
+            console.log(result);
+            res.status(201).json(
+                result
+            );
+        } catch (error) {
+            res.status(400).json({
+                message: error.message
+            });
         }
-    });
+    }
+});
 
-    // DELETE Request
-    router.delete('/:id', async (req, res) => {
-        if (req.method === "DELETE") {
-            const id = req.params.id;
-            try {
-                const result = await DB_COLLECTIONS.deleteOne({
-                    _id: new ObjectId(id)
-                });
-                if (result.deletedCount === 0) {
-                    return res.status(404).json({
-                        message: "Cannot find student"
-                    });
-                }
-                res.json({
-                    message: "Student Entry Deleted"
-                });
-            } catch (error) {
-                res.status(500).json({
-                    message: error.message
+// PUT Request
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    if (req.method === "PUT" && ObjectId.isValid(id)) {
+        const updates = {
+            name: req.body.name,
+            scores: req.body.scores
+        };
+        try {
+            const result = await DB_COLLECTIONS.replaceOne(
+                { _id: new ObjectId(id) },
+                updates
+            );
+            if (result.matchedCount === 0) {
+                return res.status(404).json({
+                    message: 'Cannot find student'
                 });
             }
-        }
-    });
 
-    return router;
+            const student = await DB_COLLECTIONS.findOne({
+                _id: new ObjectId(id)
+            });
+            if (student === null) {
+                return res.status(404).json({
+                    message: "Could not Find Student"
+                });
+            }
+            res.json(student);
+        }
+        catch (error) {
+            res.status(400).json({
+                message: error.message
+            });
+        }
+    }
+});
+
+// PATCH Request
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id;
+    if (req.method === "PATCH" && ObjectId.isValid(req.params.id)) {
+        const updates = req.body;
+        try {
+            const result = await DB_COLLECTIONS.updateOne(
+                { _id: new ObjectId(id) },
+                {$set : updates}
+            );
+            const student = await DB_COLLECTIONS.findOne({
+                _id: new ObjectId(id)
+            });
+            if (student === null) {
+                return res.status(404).json({
+                    message: "Could not Find Student"
+                });
+            }
+            res.json(student);
+        }
+        catch (error) {
+            res.status(400).json({
+                message: error.message
+            });
+        }
+    }
+});
+
+// DELETE Request
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    if (req.method === "DELETE" && ObjectId.isValid(req.params.id)) {
+        try {
+            const result = await DB_COLLECTIONS.deleteOne({
+                _id: new ObjectId(id)
+            });
+            if (result.deletedCount === 0) {
+                return res.status(404).json({
+                    message: "Cannot find student"
+                });
+            }
+            res.json({
+                message: "Student Entry Deleted"
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+        }
+    }
+});
+
+return router;
 }
 
 module.exports = studentRouter;
